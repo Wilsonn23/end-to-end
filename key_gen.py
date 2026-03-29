a6328269ba7b5d82a1f43ce11ca74aae071f7970
Wilson 23 <18223012@std.itb.ac.id>
2026-03-29 16:26:12 +0700

    try2

--- key_gen.py
+++ key_gen.py
@@ -12,7 +12,7 @@ def generate_keys(name):
     with open(f"{name}_public.pem", "wb") as f:
         f.write(private_key.public_key().public_bytes(
             encoding=serialization.Encoding.PEM,
-            format=serialization.PublicPublicKeyInfo.PKCS1
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
         ))
 
 generate_keys("bob") 
